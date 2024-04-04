6b74ba691c6079a83f6616a7ead0bf5b4f64fee6
manish <manish.ambekar36@gmail.com>
2024-04-04 21:16:49 +0530

    create s3 bucket

---
+++ backend.tf
@@ -0,0 +1,14 @@
+# create s3
+resource "aws_s3_bucket" "project_bucket"{
+    bucket = "mca_project_bucket"
+    versioning {
+      enabled = true
+    }
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+}
