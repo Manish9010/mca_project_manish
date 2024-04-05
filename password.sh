79b121d6958b294061466e4c0757bbc7b3207987
manish <manish.ambekar36@gmail.com>
2024-04-06 01:06:36 +0530

    update

--- password.sh
+++ password.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Get the instance IDs from Terraform
+instance_ids=$(terraform output -raw instance_ids)
+
+# Convert the string to an array
+IFS=$'\n' read -rd '' -a instance_ids <<< "$instance_ids"
+
+
 # Array containing the instance IDs
-instance_ids=("i-090d1cc887c8a5c9b" "i-0dcb9eabdaa19169d" "i-041912a89df35baaf")
+#instance_ids=("i-090d1cc887c8a5c9b" "i-0dcb9eabdaa19169d" "i-041912a89df35baaf")
 
 # Path to the private key file
 private_key="keys.pem"
